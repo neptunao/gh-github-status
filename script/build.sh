5a628629fed88c125208963b9d21d81451322f16
Alexander Ryabets <naeptunao@gmail.com>
2022-07-28 18:59:50 +0200

    Create main.yml

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 
+mkdir "$GITHUB_WORKSPACE/dist"
 cross build --release --target x86_64-pc-windows-gnu
-#cross build --release --target x86_64-unknown-linux-gnu
+mv "$GITHUB_WORKSPACE/target/x86_64-pc-windows-gnu/release/gh-github-status.exe" "$GITHUB_WORKSPACE/dist/gh-github-status_windows_amd64.exe"
+
+cross build --release --target x86_64-unknown-linux-gnu
+mv "$GITHUB_WORKSPACE/target/x86_64-unknown-linux-gnu/release/gh-github-status" "$GITHUB_WORKSPACE/dist/gh-github-status_linux_amd64"
