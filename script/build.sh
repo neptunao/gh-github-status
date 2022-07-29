943f8867bb3fd11392e66f630f00d385793380cc
Alexander Ryabets <naeptunao@gmail.com>
2022-07-29 15:21:49 +0200

    Add macos build

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir "$GITHUB_WORKSPACE/dist"
 cross build --release --target x86_64-pc-windows-gnu
 mv "$GITHUB_WORKSPACE/target/x86_64-pc-windows-gnu/release/gh-github-status.exe" "$GITHUB_WORKSPACE/dist/gh-github-status_windows_amd64.exe"
 
