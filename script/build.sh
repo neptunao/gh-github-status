6f494e71e86beedc50f1100924ce0fbcc2bce5d9
Alexander Ryabets <naeptunao@gmail.com>
2022-07-29 17:12:02 +0200

    Added assets

--- script/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-cross build --release --target x86_64-pc-windows-gnu
-mv "$GITHUB_WORKSPACE/target/x86_64-pc-windows-gnu/release/gh-github-status.exe" "$GITHUB_WORKSPACE/dist/gh-github-status-windows-amd64.exe"
-
-cross build --release --target x86_64-unknown-linux-gnu
-mv "$GITHUB_WORKSPACE/target/x86_64-unknown-linux-gnu/release/gh-github-status" "$GITHUB_WORKSPACE/dist/gh-github-status-linux-amd64"
