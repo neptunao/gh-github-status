42158220a08aac64421f4ac6af7f693b5f0d7139
Alexander Ryabets <naeptunao@gmail.com>
2022-07-29 15:41:07 +0200

    publish for tag

--- script/build.sh
+++ script/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 cross build --release --target x86_64-pc-windows-gnu
-mv "$GITHUB_WORKSPACE/target/x86_64-pc-windows-gnu/release/gh-github-status.exe" "$GITHUB_WORKSPACE/dist/gh-github-status_windows_amd64.exe"
+mv "$GITHUB_WORKSPACE/target/x86_64-pc-windows-gnu/release/gh-github-status.exe" "$GITHUB_WORKSPACE/dist/gh-github-status-windows-amd64.exe"
 
 cross build --release --target x86_64-unknown-linux-gnu
-mv "$GITHUB_WORKSPACE/target/x86_64-unknown-linux-gnu/release/gh-github-status" "$GITHUB_WORKSPACE/dist/gh-github-status_linux_amd64"
+mv "$GITHUB_WORKSPACE/target/x86_64-unknown-linux-gnu/release/gh-github-status" "$GITHUB_WORKSPACE/dist/gh-github-status-linux-amd64"
